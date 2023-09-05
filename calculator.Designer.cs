c08114fda53a1e89b8c7b16b658ce8b765b96a4a
Zhafran Dzaky <zhafrandzaky@gmail.com>
2023-09-06 01:45:08 +0700

    feat: initial commit

---
+++ calculator.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Calculator
+{
+    partial class calculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(calculator));
+            TxtBox = new TextBox();
+            ClearBtn = new Button();
+            MinusPlusBtn = new Button();
+            ModuleBtn = new Button();
+            DivideBtn = new Button();
+            SevenBtn = new Button();
+            EightBtn = new Button();
+            NineBtn = new Button();
+            MultiplyBtn = new Button();
+            FourBtn = new Button();
+            FiveBtn = new Button();
+            SixBtn = new Button();
+            MinusBtn = new Button();
+            OneBtn = new Button();
+            TwoBtn = new Button();
+            ThreeBtn = new Button();
+            PlusBtn = new Button();
+            ZeroBtn = new Button();
+            DotBtn = new Button();
+            EqualBtn = new Button();
+            SuspendLayout();
+            // 
+            // TxtBox
+            // 
+            resources.ApplyResources(TxtBox, "TxtBox");
+            TxtBox.Name = "TxtBox";
+            TxtBox.ReadOnly = true;
+            TxtBox.TextChanged += TxtBox_TextChanged;
+            // 
+            // ClearBtn
+            // 
+            ClearBtn.BackColor = SystemColors.ControlDarkDark;
+            resources.ApplyResources(ClearBtn, "ClearBtn");
+            ClearBtn.ForeColor = Color.White;
+            ClearBtn.Name = "ClearBtn";
+            ClearBtn.UseVisualStyleBackColor = false;
+            ClearBtn.Click += ClearBtn_Click;
+            // 
+            // MinusPlusBtn
+            // 
+            MinusPlusBtn.BackColor = SystemColors.ControlDarkDark;
+            resources.ApplyResources(MinusPlusBtn, "MinusPlusBtn");
+            MinusPlusBtn.ForeColor = Color.White;
+            MinusPlusBtn.Name = "MinusPlusBtn";
+            MinusPlusBtn.UseVisualStyleBackColor = false;
+            MinusPlusBtn.Click += MinusPlusBtn_Click;
+            // 
+            // ModuleBtn
+            // 
+            ModuleBtn.BackColor = SystemColors.ControlDarkDark;
+            resources.ApplyResources(ModuleBtn, "ModuleBtn");
+            ModuleBtn.ForeColor = Color.White;
+            ModuleBtn.Name = "ModuleBtn";
+            ModuleBtn.UseVisualStyleBackColor = false;
+            ModuleBtn.Click += ModuleBtn_Click;
+            // 
+            // DivideBtn
+            // 
+            DivideBtn.BackColor = Color.SandyBrown;
+            resources.ApplyResources(DivideBtn, "DivideBtn");
+            DivideBtn.Name = "DivideBtn";
+            DivideBtn.UseVisualStyleBackColor = false;
+            DivideBtn.Click += DivideBtn_Click;
+            // 
+            // SevenBtn
+            // 
+            resources.ApplyResources(SevenBtn, "SevenBtn");
+            SevenBtn.Name = "SevenBtn";
+            SevenBtn.UseVisualStyleBackColor = true;
+            SevenBtn.Click += SevenBtn_Click;
+            // 
+            // EightBtn
+            // 
+            resources.ApplyResources(EightBtn, "EightBtn");
+            EightBtn.Name = "EightBtn";
+            EightBtn.UseVisualStyleBackColor = true;
+            EightBtn.Click += EightBtn_Click;
+            // 
+            // NineBtn
+            // 
+            resources.ApplyResources(NineBtn, "NineBtn");
+            NineBtn.Name = "NineBtn";
+            NineBtn.UseVisualStyleBackColor = true;
+            NineBtn.Click += NineBtn_Click;
+            // 
+            // MultiplyBtn
+            // 
+            MultiplyBtn.BackColor = Color.SandyBrown;
+            resources.ApplyResources(MultiplyBtn, "MultiplyBtn");
+            MultiplyBtn.Name = "MultiplyBtn";
+            MultiplyBtn.UseVisualStyleBackColor = false;
+            MultiplyBtn.Click += MultiplyBtn_Click;
+            // 
+            // FourBtn
+            // 
+            resources.ApplyResources(FourBtn, "FourBtn");
+            FourBtn.Name = "FourBtn";
+            FourBtn.UseVisualStyleBackColor = true;
+            FourBtn.Click += FourBtn_Click;
+            // 
+            // FiveBtn
+            // 
+            resources.ApplyResources(FiveBtn, "FiveBtn");
+            FiveBtn.Name = "FiveBtn";
+            FiveBtn.UseVisualStyleBackColor = true;
+            FiveBtn.Click += FiveBtn_Click;
+            // 
+            // SixBtn
+            // 
+            resources.ApplyResources(SixBtn, "SixBtn");
+            SixBtn.Name = "SixBtn";
+            SixBtn.UseVisualStyleBackColor = true;
+            SixBtn.Click += SixBtn_Click;
+            // 
+            // MinusBtn
+            // 
+            MinusBtn.BackColor = Color.SandyBrown;
+            resources.ApplyResources(MinusBtn, "MinusBtn");
+            MinusBtn.Name = "MinusBtn";
+            MinusBtn.UseVisualStyleBackColor = false;
+            MinusBtn.Click += MinusBtn_Click;
+            // 
+            // OneBtn
+            // 
+            resources.ApplyResources(OneBtn, "OneBtn");
+            OneBtn.Name = "OneBtn";
+            OneBtn.UseVisualStyleBackColor = true;
+            OneBtn.Click += OneBtn_Click;
+            // 
+            // TwoBtn
+            // 
+            resources.ApplyResources(TwoBtn, "TwoBtn");
+            TwoBtn.Name = "TwoBtn";
+            TwoBtn.UseVisualStyleBackColor = true;
+            TwoBtn.Click += TwoBtn_Click;
+            // 
+            // ThreeBtn
+            // 
+            resources.ApplyResources(ThreeBtn, "ThreeBtn");
+            ThreeBtn.Name = "ThreeBtn";
+            ThreeBtn.UseVisualStyleBackColor = true;
+            ThreeBtn.Click += ThreeBtn_Click;
+            // 
+            // PlusBtn
+            // 
+            PlusBtn.BackColor = Color.SandyBrown;
+            resources.ApplyResources(PlusBtn, "PlusBtn");
+            PlusBtn.Name = "PlusBtn";
+            PlusBtn.UseVisualStyleBackColor = false;
+            PlusBtn.Click += PlusBtn_Click;
+            // 
+            // ZeroBtn
+            // 
+            resources.ApplyResources(ZeroBtn, "ZeroBtn");
+            ZeroBtn.Name = "ZeroBtn";
+            ZeroBtn.UseVisualStyleBackColor = true;
+            ZeroBtn.Click += ZeroBtn_Click;
+            // 
+            // DotBtn
+            // 
+            resources.ApplyResources(DotBtn, "DotBtn");
+            DotBtn.Name = "DotBtn";
+            DotBtn.UseVisualStyleBackColor = true;
+            DotBtn.Click += DotBtn_Click;
+            // 
+            // EqualBtn
+            // 
+            EqualBtn.BackColor = Color.SandyBrown;
+            resources.ApplyResources(EqualBtn, "EqualBtn");
+            EqualBtn.Name = "EqualBtn";
+            EqualBtn.UseVisualStyleBackColor = false;
+            EqualBtn.Click += EqualBtn_Click;
+            // 
+            // calculator
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(EqualBtn);
+            Controls.Add(DotBtn);
+            Controls.Add(ZeroBtn);
+            Controls.Add(PlusBtn);
+            Controls.Add(ThreeBtn);
+            Controls.Add(TwoBtn);
+            Controls.Add(OneBtn);
+            Controls.Add(MinusBtn);
+            Controls.Add(SixBtn);
+            Controls.Add(FiveBtn);
+            Controls.Add(FourBtn);
+            Controls.Add(MultiplyBtn);
+            Controls.Add(NineBtn);
+            Controls.Add(EightBtn);
+            Controls.Add(SevenBtn);
+            Controls.Add(DivideBtn);
+            Controls.Add(ModuleBtn);
+            Controls.Add(MinusPlusBtn);
+            Controls.Add(ClearBtn);
+            Controls.Add(TxtBox);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "calculator";
+            Load += calculator_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox TxtBox;
+        private Button ClearBtn;
+        private Button MinusPlusBtn;
+        private Button ModuleBtn;
+        private Button DivideBtn;
+        private Button SevenBtn;
+        private Button EightBtn;
+        private Button NineBtn;
+        private Button MultiplyBtn;
+        private Button FourBtn;
+        private Button FiveBtn;
+        private Button SixBtn;
+        private Button MinusBtn;
+        private Button OneBtn;
+        private Button TwoBtn;
+        private Button ThreeBtn;
+        private Button PlusBtn;
+        private Button ZeroBtn;
+        private Button DotBtn;
+        private Button EqualBtn;
+    }
+}
